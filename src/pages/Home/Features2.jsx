236a28f8f23b714fd04287dbd10fab1c98b281a2
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-10-06 19:39:58 +0100

    refactor Features2 component for improved layout and styling

--- src/pages/Home/Features2.jsx
+++ src/pages/Home/Features2.jsx
@@ -1,17 +1,53 @@
-import React from 'react'
-import Feature2 from '../../images/feature2.png'
+import React from "react";
+import Feature2 from "../../images/feature2.png";
+import { GoDash } from "react-icons/go";
+import { CiCircleCheck } from "react-icons/ci";
 
 const Features2 = () => {
   return (
-    <section className='bg-bg-darkest text-white w-full'>
-        <div className='grid md:flex md:items-center gap-8 px-6 md:px-24 py-10 w-full'>
-            <div>
-                <img src={Feature2} alt="" />
-            </div>
+    <section className="bg-bg-darkest text-white w-full">
+      <div className="grid md:flex md:flex-row-reverse md:items-center gap-8 px-6 md:px-24 py-10 w-full">
+        <div>
+          <img src={Feature2} alt="" className="md:w-159" />
         </div>
-    </section>
-    
-  )
-}
+        <div>
+          <div>
+            <div className="flex items-center space-x-2">
+              <GoDash />
+              <h4 className="text-sm md:text-xl text-bg-soft font-normal md:font-medium">ALL FEATURES</h4>
+            </div>
 
-export default Features2
+            <h1 className="text-3xl md:text-5xl font-semibold mt-4">
+              Video calls loved by extraordinary teams.
+            </h1>
+            <p className="text-xl md:text-2xl font-normal text-bg-faint mt-6">
+              Making hybrid‑conference inclusive with unique audio technology.
+            </p>
+          </div>
+          <div className="mt-10">
+            <div className="flex items-center space-x-2">
+              <CiCircleCheck className="text-accent-light" />
+              <p className="text-sm md:text-xl text-bg-faint">
+                High-quality video and audio
+              </p>
+            </div>
+            <div className="flex items-center space-x-2">
+              <CiCircleCheck className="text-accent-light" />
+              <p className="text-sm md:text-xl text-bg-faint">
+                Screen sharing and collaboration tools
+              </p>
+            </div>
+            <div className="flex items-center space-x-2">
+              <CiCircleCheck className="text-accent-light" />
+              <p className="text-sm md:text-xl text-bg-faint">
+                Customizable virtual backgrounds
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Features2;
