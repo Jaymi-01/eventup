af5c8a8b4c32efb759246f2d06292a3e3acc2a75
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-10-06 18:55:58 +0100

    wip

---
+++ src/pages/Home/Features2.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Feature2 from '../../images/feature2.png'
+
+const Features2 = () => {
+  return (
+    <section className='bg-bg-darkest text-white w-full'>
+        <div className='grid md:flex md:items-center gap-8 px-6 md:px-24 py-10 w-full'>
+            <div>
+                <img src={Feature2} alt="" />
+            </div>
+        </div>
+    </section>
+    
+  )
+}
+
+export default Features2
