bc43d2a1e11c0a0b2176b18081c16f45409122f8
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-10-06 18:55:45 +0100

    refactor App component to implement routing and structure layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import Navbar from './components/Navbar.jsx'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+// import Footer from "./components/Footer.jsx";
+import Home from "./pages/Home/Index.jsx";
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <Router>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        {/* <Footer /> */}
+      </div>
+    </Router>
   );
-}
+};
 
-export default App
+export default App;
