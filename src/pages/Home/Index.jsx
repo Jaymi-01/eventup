0b35b7fba1ec05699b428341e9a8b6b1bfdaef2c
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-10-06 18:55:34 +0100

    add Index component to structure the Home page layout

---
+++ src/pages/Home/Index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './Header.jsx'
+import Clients from './Clients.jsx'
+import About from './About.jsx'
+import Features from './Features.jsx'
+import Features2 from './Features2.jsx'
+
+const Index = () => {
+  return (
+    <div>
+      <Header />
+      <Clients />
+      <About />
+      <Features />
+      <Features2 />
+    </div>
+  )
+}
+
+export default Index
