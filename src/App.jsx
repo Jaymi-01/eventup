9566a1038ac9d331a0cffe7e6bbea669597f5280
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-10-07 08:37:52 +0100

    add About route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
 import Home from "./pages/Home/Index.jsx";
+import About from './pages/About/Index.jsx';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
         <Footer />
       </div>
