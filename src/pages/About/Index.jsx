48d219f0420180f503ea2f73dd9c30b6646d5f56
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-10-07 14:40:52 +0100

    add About and Team components to About page

--- src/pages/About/Index.jsx
+++ src/pages/About/Index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from './Header.jsx'
+import About from './About.jsx'
+import Team from './Team.jsx'
 
 const Index = () => {
   return (
     <div>
         <Header />
+        <About />
+        <Team />
     </div>
   )
 }
