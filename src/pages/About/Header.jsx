b6c6ef4b75975ad155a6647d1d826e62be3a1083
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-10-07 08:39:23 +0100

    add Header component for About page

---
+++ src/pages/About/Header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Conference from "../../images/conference.png";
+
+const Header = () => {
+  return (
+    <section className="bg-bg-darkest text-white pt-25 overflow-hidden">
+      <div className="px-4 md:px-10">
+        <div className="flex flex-col md:flex-row gap-4 md:gap-20 justify-between mb-8 pr-4">
+          <h1 className="text-4xl md:text-5xl font-semibold">
+            Experience You Can Trust
+          </h1>
+          <p className="mt-4 text-bg-pale text-lg md:text-2xl font-normal">
+            From year to year, we strive to invest in the most innovative
+            technology that is used by both small enterprises and space
+            enterprises
+          </p>
+        </div>
+      </div>
+
+      {/* Full-width image */}
+      <div className="w-full">
+        <img
+          src={Conference}
+          alt="Conference"
+          className="w-full h-auto object-cover"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Header;
