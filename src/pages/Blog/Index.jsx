73a20d21ef0a94f172cf3e0a524b41eda7e15e3d
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2026-01-07 14:29:20 +0100

    feat: create Index component to render BlogComponent

---
+++ src/pages/Blog/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BlogComponent from './Blog.jsx'
+
+const Index = () => {
+  return (
+    <div>
+      <BlogComponent />
+    </div>
+  )
+}
+
+export default Index
