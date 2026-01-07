4777482ddae2475646fe387ea0199c667ef15a56
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2026-01-07 14:30:54 +0100

    feat: create Index component for Career page

---
+++ src/pages/Career/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CareerComponent from './Career.jsx'
+
+const Index = () => {
+  return (
+    <div>
+      <CareerComponent />
+    </div>
+  )
+}
+
+export default Index
